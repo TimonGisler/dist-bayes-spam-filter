36c199ef013c57e2a834de489a72b0629f0b142a
timon <timongisler@outlook.com>
2024-10-10 18:52:19 +0200

    added test data and repository

---
+++ src/main/java/org/example/model/MailRepository.java
@@ -0,0 +1,16 @@
+package org.example.model;
+
+import java.util.List;
+
+/**
+ * Reads in the mails from the files
+ */
+public class MailRepository {
+
+    public List<Mail> getAllHamAnlernMails() {
+
+
+        return null;
+    }
+
+}
